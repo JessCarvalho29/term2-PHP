422ca44a3317d960637d3960f2ce42a7092d9827
Jessica <jessicacarvalho.rosendo@gmail.com>
2024-06-06 20:29:38 -0400

    Including the SQL

---
+++ assignment1/sql/Assignment 1 - PHP.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE pizza_delivery;.
+
+USE pizza_delivery;
+
+CREATE TABLE orders (
+	order_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	quantity VARCHAR(3) NOT NULL,
+	size_pizza VARCHAR(20) NOT NULL,
+	shape VARCHAR(20) NOT NULL,
+	topping VARCHAR(20) NOT NULL, 
+	crust VARCHAR(20) NOT NULL, 
+	delivery_takeout VARCHAR(20) NOT NULL, 
+	client_name VARCHAR(70) NOT NULL, 
+	phone VARCHAR(15) NOT NULL );
+	
+INSERT INTO orders (quantity, size_pizza, shape, topping, crust, delivery_takeout, client_name, phone)
+	VALUES 	('1', 'small', 'square', 'pepperoni', 'thick', 'delivery', 'Jessica Carvalho',	'647787444'),
+	('2', 'medium', 'round', 'bacon', 'stuffed', 'takeout	', 'Joana',	'688887444'),
+	('1', 'large', 'round', 'ham', 'thin', 'takeout	', 'Tiago',	'688887444');
+
+SELECT * FROM orders;
+
